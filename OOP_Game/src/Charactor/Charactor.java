cd2af2e4f1983fc777dbfc65f928fb6b13afcf51
pangpntt <88421714+pangpntt@users.noreply.github.com>
2022-12-13 00:51:34 +0700

    diagram

---
+++ OOP_Game/src/Charactor/Charactor.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Charactor;
+
+/**
+ *
+ * @author pangpntt
+ */
+public class Charactor {
+    private int height;
+    private int weight;
+    private int positionX;
+    private int positionY;
+    public void setHeight(int height){
+        this.height = height;
+    }
+    public int getHeight(){
+        return this.height;
+    }
+    public void setWeight(int weight){
+        this.weight = weight;
+    }
+    public int getWeight(){
+        return this.weight;
+    }
+    public void setPositionX(int positionX){
+        this.positionX = positionX;
+    }
+    public int getPositionX(){
+        return this.positionX;
+    }
+    public void setPositionY(int positionY){
+        this.positionY = positionY;
+    }
+    public int getPositionY(){
+        return this.positionY;
+    }
+}
