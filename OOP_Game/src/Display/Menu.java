fe5adc86790069cb00931db7f7d7890ffefbadf9
pangpntt <88421714+pangpntt@users.noreply.github.com>
2022-12-13 00:48:49 +0700

    diagram eiei

---
+++ OOP_Game/src/Display/Menu.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Display;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author pangpntt
+ */
+public class Menu{
+    private JPanel jpMenu;
+    private JButton buttonStart, buttonSystems, buttonExit;
+    private JInternalFrame selectFrame, SystemsFrame;
+    public void Menu(){
+        
+    }
+    public void changeToSystem(){
+        
+    }
+}
