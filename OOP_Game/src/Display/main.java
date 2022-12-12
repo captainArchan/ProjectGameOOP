e270fadc6ddefe16def3195f9e5f9502681fe013
pangpntt <88421714+pangpntt@users.noreply.github.com>
2022-12-12 23:46:06 +0700

    test

--- OOP_Game/src/Display/main.java
+++ OOP_Game/src/Display/main.java
@@ -5,7 +5,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stu
 		// ettdadgthrqr
-		System.out.println("te");
+		System.out.println("test");
 	}
 
 }
