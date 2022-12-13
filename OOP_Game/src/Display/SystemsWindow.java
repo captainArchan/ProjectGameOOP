783dc35fb580774ee80a127b07c3719f64eaacb7
ArchanPhalanuphat <archan.phalanuphat@gmail.com>
2022-12-13 22:48:02 +0700

    add img

--- OOP_Game/src/Display/SystemsWindow.java
+++ OOP_Game/src/Display/SystemsWindow.java
@@ -5,6 +5,12 @@
 package Display;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.print.DocFlavor.URL;
 import javax.swing.*;
 
 /**
@@ -13,60 +19,78 @@ import javax.swing.*;
  */
 public class SystemsWindow {
 
-    private JInternalFrame jpSystems;
-    private JDesktopPane dp;
-    private JPanel jpsound, jpback;
-    private JLabel sound;
-    private JButton back;
-    private JSlider s;
-    private JFrame fr;
+	private JInternalFrame jpSystems;
+	private JDesktopPane dp;
+	private JPanel jpsound, jpback, jpSlider, picLabel;
+	private JLabel sound, imgSound;
+	private JButton back;
+	private JSlider slider;
+	private JFrame fr;
+	private BufferedImage wPic;
 
-    public SystemsWindow() {
-        fr = new JFrame();
-        jpSystems = new JInternalFrame("test", true, true, true, true);
-        jpsound = new JPanel();
-        jpback = new JPanel();
-        sound = new JLabel("SOUND", SwingConstants.CENTER);
-        back = new JButton("BACK");
-        s = new JSlider();
-        dp = new JDesktopPane();
-        Font myFont = new Font("Monospaced", Font.BOLD, 80);
-        Font myFont2 = new Font("Monospaced", Font.BOLD, 30);
-        sound.setFont(myFont);
-        back.setFont(myFont2);
-        
-        fr.setLayout(new BorderLayout());
-        jpSystems.setLayout(new BorderLayout());
-        jpsound.setLayout(new GridLayout(2, 1));
-        jpsound.add(sound);
-        jpsound.add(s);
-        jpSystems.getContentPane().add(jpsound, BorderLayout.NORTH);
-        
-        jpback.setLayout(new FlowLayout(FlowLayout.RIGHT));
-        jpback.add(back);
-        jpSystems.getContentPane().add(jpback);
-        
-        dp.add(jpSystems);
-        fr.add(dp);
-        jpSystems.setSize(700, 300);
-        fr.setSize(1240, 750);
-        fr.add(dp);
-        fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jpSystems.setLocation(250, 200);
-        jpSystems.setVisible(true);
-        fr.setLocationRelativeTo(null);
-        fr.setVisible(true);
-    }
+	public SystemsWindow() {
+		fr = new JFrame();
+		jpSystems = new JInternalFrame("test", true, true, true, true);
+		jpsound = new JPanel();
+		jpback = new JPanel();
+		jpSlider = new JPanel();
 
-    public static void main(String[] args) {
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        SwingUtilities.invokeLater(() -> {
-            SystemsWindow frame = new SystemsWindow();
-        });
-        
-    }
+		
+		sound = new JLabel("SOUND", SwingConstants.CENTER);
+		back = new JButton("BACK");
+		back.setBackground(new java.awt.Color(17, 20, 20));
+		back.setForeground(Color.RED);
+		
+		slider = new JSlider();
+		dp = new JDesktopPane();
+		
+		try {
+			wPic = ImageIO.read(this.getClass().getResource("/Image/sound.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		imgSound = new JLabel(new ImageIcon(new ImageIcon(wPic).getImage().getScaledInstance(80, 80, Image.SCALE_DEFAULT)));
+		Font myFont = new Font("Monospaced", Font.BOLD, 80);
+		Font myFont2 = new Font("Monospaced", Font.BOLD, 30);
+		sound.setFont(myFont);
+		back.setFont(myFont2);
+		jpSlider.setLayout(new BorderLayout());
+		
+		jpSlider.setLayout(new BorderLayout());
+		jpSlider.add(imgSound, BorderLayout.WEST);
+		jpSlider.add(slider, BorderLayout.CENTER);
+		fr.setLayout(new BorderLayout());
+		jpSystems.setLayout(new BorderLayout()); 
+		jpsound.setLayout(new GridLayout(2, 1));
+		jpsound.add(sound);
+		jpsound.add(jpSlider);
+		jpSystems.getContentPane().add(jpsound, BorderLayout.NORTH);
+
+		jpback.setLayout(new FlowLayout(FlowLayout.RIGHT));
+		jpback.add(back);
+		jpSystems.getContentPane().add(jpback);
+		
+		dp.add(jpSystems);
+		fr.add(dp);
+		jpSystems.setSize(700, 300);
+		fr.setSize(1240, 750);
+		fr.add(dp);
+		fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jpSystems.setLocation(250, 200);
+		jpSystems.setVisible(true);
+		fr.setLocationRelativeTo(null);
+		fr.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		SwingUtilities.invokeLater(() -> {
+			SystemsWindow frame = new SystemsWindow();
+		});
+
+	}
 }
--- OOP_Game/src/module-info.java
+++ OOP_Game/src/module-info.java
@@ -7,4 +7,5 @@
  */
 module OOP_Game {
 requires java.desktop;
+
 }
