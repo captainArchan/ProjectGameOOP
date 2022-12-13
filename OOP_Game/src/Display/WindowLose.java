0b7f1a048948fcf14e77ecc6f382fcb3690b6e4c
pangpntt <88421714+pangpntt@users.noreply.github.com>
2022-12-14 00:50:18 +0700

    lose gui

--- OOP_Game/src/Display/WindowLose.java
+++ OOP_Game/src/Display/WindowLose.java
@@ -4,6 +4,11 @@
  */
 package Display;
 
+import java.awt.*;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 /**
@@ -11,10 +16,51 @@ import javax.swing.*;
  * @author pangpntt
  */
 public class WindowLose extends extraWindow {
-    private JPanel jpLose;
-    private JButton buttonRestart, ButtonBackToMenu;
-    private JTextField textLose;
-    public void Lose(){
+    private JFrame fr;
+    private JInternalFrame jpLose;
+    private JDesktopPane dp;
+    private JPanel jptxt, jpbutton;
+    private JButton buttonRestart, buttonBackToMenu;
+    private JLabel textLose;
+    public WindowLose(){
+        fr = new JFrame();
+        jpLose = new JInternalFrame("lose", true, true, true, true);
+        dp = new JDesktopPane();
+        jptxt = new JPanel();
+        jpbutton = new JPanel();
+        buttonRestart = new JButton("RESTART");
+        buttonBackToMenu = new JButton("MENU");
+        textLose = new JLabel("<html>GAME<br/>OVER</html>", SwingConstants.CENTER);
         
+        Font myFont = new Font("Monospaced", Font.BOLD, 100);
+        Font myFont2 = new Font("Monospaced", Font.BOLD, 30);
+        textLose.setFont(myFont);
+        buttonRestart.setFont(myFont2);
+        buttonBackToMenu.setFont(myFont2);
+                
+        fr.setLayout(new BorderLayout());
+        jpLose.setLayout(new BorderLayout());
+        jptxt.setLayout(new GridLayout(2,1));
+        jpbutton.setLayout(new FlowLayout());
+        
+        jpbutton.add(buttonRestart);
+        jpbutton.add(buttonBackToMenu);
+        jptxt.add(textLose);
+        jptxt.add(jpbutton);
+        jpLose.add(jptxt);
+        dp.add(jpLose);
+        fr.add(dp);
+        
+        jpLose.setSize(500, 450);
+        jpLose.setLocation(350, 100);
+        jpLose.setVisible(true);
+        fr.setSize(1240, 750);
+        fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        fr.setLocationRelativeTo(null);
+        fr.setVisible(true);
+    }
+    
+    public static void main(String[] args) {
+        new WindowLose();
     }
 }
